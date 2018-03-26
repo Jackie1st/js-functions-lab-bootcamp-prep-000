b9b71e20e7c29a8e1d49613d1bb99cf97337c272
Jackie First <jackiejeep@gmail.com>
2018-03-26 23:13:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,6 @@ function happyHolidaysTo() {
 function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}`;
 }
+function holidayCountdown(days, holiday) {
+  return `It's ${days} days until ${holiday}`;
+}
